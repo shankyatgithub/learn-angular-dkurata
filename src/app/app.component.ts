eb6bd26eeff467bf781810b5a552296c03c6ce95
Shashank Kadge <shashank_kadge@hotmail.com>
2022-09-29 23:16:17 -0400

    bindings types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
  selector: 'shanks-root',
- template: `
-  <div><h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
-  </div>
- `
+ templateUrl: './app.component.html'
 })
 
 export class AppComponent{
